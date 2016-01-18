107e709c25f659aecd24371b4e792ce01fcdc671
Nathan Riemer <nathan.riemer@gmail.com>
2016-01-18 16:52:12 -0500

    Adds protractor tests

--- tests/index_spec.js
+++ tests/index_spec.js
@@ -1,14 +1,15 @@
 describe('visiting the site', function(){
   it('greets the user', function(){
     browser.get('http://localhost:3000')
-    var h1 = element(by.css('.splash'));
-    expect(h1.getText()).toEqual("What's up, dog?");
+    var h2 = element(by.id('welcome'));
+    expect(h2.getText()).toEqual("Welcome to Birdwatchr!");
   });
-  it('adds a dog', function(){
+  it('adds a sighting', function(){
     browser.get('http://localhost:3000');
-    element(by.model('dogs.new')).sendKeys('Jerry');
+    element(by.model('sightings.newSighting.type')).sendKeys('Pigeon');
+    element(by.model('sightings.newSighting.location')).sendKeys('New York, NY');
     element(by.id('save')).click();
-    var dogsList = element.all(by.repeater('dog in dogs.all'));
-    expect(dogsList.last().getText()).toEqual('Jerry');
+    var sightingsList = element.all(by.repeater('sighting in sightings.all'));
+    expect(sightingsList.first().element(by.binding('sighting.type')).getText()).toEqual('Pigeon spotted at New York, NY');
   })
 });
